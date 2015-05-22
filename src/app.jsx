ea01efbf78fde2ea7e75774e69b9a87554520e9c
Massimo Iacolare <info@massimoiacolare.it>
2015-05-22 22:53:35 +0200

    Layout tweaks

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,9 @@
 var React = require("react");
 
 var App = React.createClass({
-    render() {
-        return <h1>Hello React</h1>;
+    render: function() {
+        return <h1>Hello React :-)</h1>;
     }
-})
+});
 
 React.render(<App/>, document.getElementById('example'));
